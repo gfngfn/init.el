b35a02ca83f6f12fcde3180f01129c76099c99a2
Takashi SUWA <bd.gfngfn@gmail.com>
2019-04-04 14:14:03 +0900

    move the description of key binds and non-distributed packages in 'init.el'

--- init.el
+++ init.el
@@ -32,7 +32,7 @@
 (menu-bar-mode -1)
 
 ;; ---- ---- font settings ---- ----
-(set-face-attribute 'default nil :family "Consolas" :height 130)
+(set-face-attribute 'default nil :family "Menlo" :height 130)
 (set-fontset-font nil 'japanese-jisx0208 (font-spec :family "Meiryo UI"))
 (setq face-font-rescale-alist '(("Meiryo UI" . 1.08)))
 
@@ -52,42 +52,9 @@
 (line-number-mode 1)
 (column-number-mode 1)
 
-;; ==== ==== ==== ==== KEY BIND ==== ==== ==== ====
-;(global-set-key [M-kanji] 'ignore)
-;(global-set-key [kanji] 'ignore)
-;; ---- ---- common ---- ----
-(define-key global-map (kbd "C-c s") 'query-replace)
-(define-key global-map (kbd "C-c a") 'beginning-of-buffer)
-(define-key global-map (kbd "C-c e") 'end-of-buffer)
-(define-key global-map (kbd "C-c t") 'shell)
-(define-key global-map (kbd "C-h") 'delete-backward-char)
-(define-key global-map (kbd "M-n") 'scroll-up-command)
-(define-key global-map (kbd "M-p") 'scroll-down-command)
-;; ---- ---- original ---- ----
-(define-key global-map (kbd "C-c m") 'gfn-macro)
-(define-key global-map (kbd "C-c l") 'gfn-insert-line)
-;; ---- ---- helm ---- ----
-(define-key global-map (kbd "C-x C-b") 'helm-buffers-list)
-(define-key global-map (kbd "C-x C-f") 'helm-find-files)
-;; ---- ---- recentf ---- ----
-(define-key global-map (kbd "C-c f") 'recentf-open-files)
-;; ---- ---- tabbar ---- ----
-(define-key global-map (kbd "M-<right>") 'tabbar-forward-tab)
-(define-key global-map (kbd "M-<left>") 'tabbar-backward-tab)
-;; ---- ---- open-junk-file ---- ----
-(define-key global-map (kbd "C-x j") 'open-junk-file)
-;; ---- ---- undo-tree ---- ----
-(define-key global-map (kbd "M-/") 'undo-tree-redo)
-(define-key global-map (kbd "C-z") 'undo-tree-undo)
 ;; ==== ==== ==== ==== OTHER SETTINGS ==== ==== ==== ====
 ;; ---- ---- garbage collection ---- ----
 (setq gc-cons-threshold (* 20 gc-cons-threshold))
-
-;; ==== ==== ==== ==== MY PACKAGES ==== ==== ==== ====
-(require 'gfn-latex)
-(require 'gfn-first)
-(require 'open-group)
-(require 'mcrd)
 
 ;; ==== ==== ==== ==== DISTRIBUTED PACKAGES ==== ==== ==== ====
 ;; ---- ---- package ---- ----
@@ -99,6 +66,20 @@
 (add-to-list 'package-archives '("org"          . "http://orgmode.org/elpa/") t)
 
 (package-initialize)
+;(custom-set-variables
+; ;; custom-set-variables was added by Custom.
+; ;; If you edit it by hand, you could mess it up, so be careful.
+; ;; Your init file should contain only one such instance.
+; ;; If there is more than one, they won't work right.
+; '(package-selected-packages
+;   (quote
+;    (auto-complete undo-tree auto-async-byte-compile tuareg open-junk-file tabbar restart-emacs helm recentf-ext))))
+;(custom-set-faces
+; ;; custom-set-faces was added by Custom.
+; ;; If you edit it by hand, you could mess it up, so be careful.
+; ;; Your init file should contain only one such instance.
+; ;; If there is more than one, they won't work right.
+; )
 
 ;; ---- ---- f ---- ----
 ;(require 'f)
@@ -146,7 +127,7 @@
            (forward-char -1)))))
 (define-key tuareg-mode-map (kbd "(") 'gfn-insert-paren-pair)
 
-(require 'install-elisp)
+;(require 'install-elisp)
 (require 'restart-emacs)
 
 ;; ---- ---- auto-async-byte-compile ---- ----
@@ -246,3 +227,37 @@
  ;; If there is more than one, they won't work right.
  )
 (put 'upcase-region 'disabled nil)
+
+;; ==== ==== ==== ==== KEY BIND ==== ==== ==== ====
+;(global-set-key [M-kanji] 'ignore)
+;(global-set-key [kanji] 'ignore)
+;; ---- ---- common ---- ----
+(define-key global-map (kbd "C-c s") 'query-replace)
+(define-key global-map (kbd "C-c a") 'beginning-of-buffer)
+(define-key global-map (kbd "C-c e") 'end-of-buffer)
+(define-key global-map (kbd "C-c t") 'shell)
+(define-key global-map (kbd "C-h") 'delete-backward-char)
+(define-key global-map (kbd "M-n") 'scroll-up-command)
+(define-key global-map (kbd "M-p") 'scroll-down-command)
+;; ---- ---- original ---- ----
+(define-key global-map (kbd "C-c m") 'gfn-macro)
+(define-key global-map (kbd "C-c l") 'gfn-insert-line)
+;; ---- ---- helm ---- ----
+(define-key global-map (kbd "C-x C-b") 'helm-buffers-list)
+(define-key global-map (kbd "C-x C-f") 'helm-find-files)
+;; ---- ---- recentf ---- ----
+(define-key global-map (kbd "C-c f") 'recentf-open-files)
+;; ---- ---- tabbar ---- ----
+(define-key global-map (kbd "M-<right>") 'tabbar-forward-tab)
+(define-key global-map (kbd "M-<left>") 'tabbar-backward-tab)
+;; ---- ---- open-junk-file ---- ----
+(define-key global-map (kbd "C-x j") 'open-junk-file)
+;; ---- ---- undo-tree ---- ----
+(define-key global-map (kbd "M-/") 'undo-tree-redo)
+(define-key global-map (kbd "C-z") 'undo-tree-undo)
+
+;; ==== ==== ==== ==== MY PACKAGES ==== ==== ==== ====
+(require 'gfn-latex)
+(require 'gfn-first)
+(require 'open-group)
+(require 'mcrd)
